c9794a4219cf9cf04590bdf51b537653aadc122d
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-11 23:18:52 -0400

    more updates

--- pages/index.js
+++ pages/index.js
@@ -6,67 +6,244 @@ import {
   CheckCircleIcon,
   OfficeBuildingIcon,
 } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 export default function Home() {
+  const tabs = [
+    { name: 'Recent', href: '#', current: true },
+    { name: 'Most Liked', href: '#', current: false },
+    { name: 'Most Answers', href: '#', current: false },
+  ]
+
+  
   return (
-    <div className="bg-white shadow">
-    <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-      <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-        <div className="flex-1 min-w-0">
-          {/* Profile */}
-          <div className="flex items-center">
-            <img
-              className="hidden h-16 w-16 rounded-full sm:block"
-              src="https://avatars.dicebear.com/api/open-peeps/placeholder.svg"
-              alt="avatra"
-            />
-            <div>
-              <div className="flex items-center">
-                <img
-                  className="h-16 w-16 rounded-full sm:hidden"
-                  src="https://avatars.dicebear.com/api/open-peeps/placeholder.svg"
-                  alt=""
-                />
-                <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-                  Good morning, Sandeep Yaramchitti
-                </h1>
+    <div>
+        <div className="bg-white shadow">
+          <div className="px-4 sm:px-6 lg:max-w-7xl lg:mx-auto lg:px-8">
+            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+              <div className="flex-1 min-w-0">
+                {/* Profile */}
+                <div className="flex items-center">
+                  <img
+                    className="hidden h-16 w-16 rounded-full sm:block"
+                    src="https://avatars.dicebear.com/api/open-peeps/placeholder.svg"
+                    alt="avatra"
+                  />
+                  <div>
+                    <div className="flex items-center">
+                      <img
+                        className="h-16 w-16 rounded-full sm:hidden"
+                        src="https://avatars.dicebear.com/api/open-peeps/placeholder.svg"
+                        alt=""
+                      />
+                      <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                        Good morning, Sandeep Yaramchitti
+                      </h1>
+                    </div>
+                    <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                      <dt className="sr-only">Company</dt>
+                      <dd className="flex items-center text-sm text-gray-500 font-medium capitalize sm:mr-6">
+                        <OfficeBuildingIcon
+                          className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        DevKit
+                      </dd>
+                      <dt className="sr-only">Account status</dt>
+                      <dd className="mt-3 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+                        <CheckCircleIcon
+                          className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                          aria-hidden="true"
+                        />
+                        Verified account
+                      </dd>
+                    </dl>
+                  </div>
+                </div>
               </div>
-              <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
-                <dt className="sr-only">Company</dt>
-                <dd className="flex items-center text-sm text-gray-500 font-medium capitalize sm:mr-6">
-                  <OfficeBuildingIcon
-                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  DevKit
-                </dd>
-                <dt className="sr-only">Account status</dt>
-                <dd className="mt-3 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
-                  <CheckCircleIcon
-                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
-                    aria-hidden="true"
-                  />
-                  Verified account
-                </dd>
-              </dl>
+              <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+                <button
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                >
+                  More Details...
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                >
+                  Create Favorites...
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-          >
-            More Details...
-          </button>
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-          >
-            Create Favorites...
-          </button>
+        <div className="flex-grow w-full max-w-7xl mx-auto xl:px-8 lg:flex pt-5">
+          <div className="flex-1 min-w-0 bg-white xl:flex">
+            <div className="bg-white lg:min-w-0 lg:flex-1">
+              <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
+                {/* Start main area*/}
+                <div className="relative h-full" style={{ minHeight: '36rem' }}>
+                  <div className="absolute inset-0 border-2 border-gray-200  rounded-lg">
+                  <nav className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
+                    {tabs.map((tab, tabIdx) => (
+                      <a
+                        key={tab.name}
+                        href={tab.href}
+                        aria-current={tab.current ? 'page' : undefined}
+                        className={classNames(
+                          tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                          tabIdx === 0 ? 'rounded-l-lg' : '',
+                          tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
+                          'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
+                        )}
+                      >
+                        <span>{tab.name}</span>
+                        <span
+                          aria-hidden="true"
+                          className={classNames(
+                            tab.current ? 'bg-rose-500' : 'bg-transparent',
+                            'absolute inset-x-0 bottom-0 h-0.5'
+                          )}
+                        />
+                      </a>
+                    ))}
+                  </nav>
+                  <div className="mt-4">
+          <h1 className="sr-only">Recent questions</h1>
+          <ul role="list" className="space-y-4">
+            <li className="bg-white px-4 py-6 shadow sm:p-6 sm:rounded-lg">
+              <article aria-labelledby="question-title-81614">
+                <div>
+                  <div className="flex space-x-3">
+                    <div className="flex-shrink-0">
+                      <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <p className="text-sm font-medium text-gray-900">
+                        <a href="#" className="hover:underline">Dries Vincent</a>
+                      </p>
+                      <p className="text-sm text-gray-500">
+                        <a href="#" className="hover:underline">
+                          <time dateTime="2020-12-09T11:43:00">December 9 at 11:43 AM</time>
+                        </a>
+                      </p>
+                    </div>
+                    <div className="flex-shrink-0 self-center flex">
+                      <div className="relative inline-block text-left">
+                        <div>
+                          <button type="button" className="-m-2 p-2 rounded-full flex items-center text-gray-400 hover:text-gray-600" id="options-menu-0-button" aria-expanded="false" aria-haspopup="true">
+                            <span className="sr-only">Open options</span>
+                            
+                            <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                              <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                            </svg>
+                          </button>
+                        </div>
+                        {/* <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="options-menu-0-button" tabIndex="-1">
+                          <div className="py-1" role="none">
+                            
+                            <a href="#" className="text-gray-700 flex px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="options-menu-0-item-0">
+                              
+                              <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                              </svg>
+                              <span>Add to favorites</span>
+                            </a>
+                            <a href="#" className="text-gray-700 flex px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="options-menu-0-item-1">
+                              
+                              <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fillRule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                              </svg>
+                              <span>Embed</span>
+                            </a>
+                            <a href="#" className="text-gray-700 flex px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="options-menu-0-item-2">
+                              
+                              <svg className="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fillRule="evenodd" d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z" clipRule="evenodd" />
+                              </svg>
+                              <span>Report content</span>
+                            </a>
+                          </div>
+                        </div> */}
+                      </div>
+                    </div>
+                  </div>
+                  <h2 id="question-title-81614" className="mt-4 text-base font-medium text-gray-900">What would you have done differently if you ran Jurassic Park?</h2>
+                </div>
+                <div className="mt-2 text-sm text-gray-700 space-y-4">
+                  <p>Jurassic Park was an incredible idea and a magnificent feat of engineering, but poor protocols and a disregard for human safety killed what could have otherwise been one of the best businesses of our generation.</p>
+                  <p>Ultimately, I think that if you wanted to run the park successfully and keep visitors safe, the most important thing to prioritize would be&hellip;</p>
+                </div>
+                <div className="mt-6 flex justify-between space-x-8">
+                  <div className="flex space-x-6">
+                    <span className="inline-flex items-center text-sm">
+                      <button type="button" className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+                      
+                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                        </svg>
+                        <span className="font-medium text-gray-900">29</span>
+                        <span className="sr-only">likes</span>
+                      </button>
+                    </span>
+                    <span className="inline-flex items-center text-sm">
+                      <button type="button" className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+    
+                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
+                        </svg>
+                        <span className="font-medium text-gray-900">11</span>
+                        <span className="sr-only">replies</span>
+                      </button>
+                    </span>
+                    <span className="inline-flex items-center text-sm">
+                      <button type="button" className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                          <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
+                        </svg>
+                        <span className="font-medium text-gray-900">2.7k</span>
+                        <span className="sr-only">views</span>
+                      </button>
+                    </span>
+                  </div>
+                  <div className="flex text-sm">
+                    <span className="inline-flex items-center text-sm">
+                      <button type="button" className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+
+                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+                        </svg>
+                        <span className="font-medium text-gray-900">Share</span>
+                      </button>
+                    </span>
+                  </div>
+                </div>
+              </article>
+            </li>
+
+          </ul>
         </div>
-      </div>
+                  </div>
+                </div>
+                {/* End main area */}
+              </div>
+            </div>
+          </div>
+          <div className="pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0  lg:border-gray-200 xl:pr-0">
+            <div className="h-full pl-6 py-6 lg:w-80">
+              {/* Start right column area */}
+              <div className="h-full relative" style={{ minHeight: '16rem' }}>
+                <div className="absolute inset-0 border-2 border-gray-200 border-dashed rounded-lg" />
+              </div>
+              {/* End right column area */}
+            </div>
+          </div>
+    </div>    
     </div>
-  </div>
   )
 }
