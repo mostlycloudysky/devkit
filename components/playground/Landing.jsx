8f6d62537c7190f77a7364b983d1a4a8a579bbba
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-14 15:44:07 -0400

    add support for github Oauth

--- components/playground/Landing.jsx
+++ components/playground/Landing.jsx
@@ -205,7 +205,6 @@ const Landing = () => {
               disabled={!code}
               className={classnames(
                 "mt-4 text-white bg-gradient-to-r from-cyan-600 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 outline-none",
-
               )}
             >
               {processing ? "Executing..." : "Submit"}
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Layout from '../components/Layout'
+import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+
   )  
 }
 
