c29321c0b913d6e38cd304588045f2dcb3df7012
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-14 23:16:22 -0400

    Update Github.jsx

--- components/alltabs/Github.jsx
+++ components/alltabs/Github.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Confetti from 'react-confetti'
 
 function Github() {
+
+  const [confettiFlag, setConfettiFlag] = useState("true")
+
+  useEffect(() => {
+    const timer = setTimeout(() => setConfettiFlag("false"), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
     <div>
-      <Confetti />
+     {confettiFlag === 'true' ? <Confetti /> : 'Confetti load is complete' }
     </div>
   )
 }
