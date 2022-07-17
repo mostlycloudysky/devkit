edb802341a55837db8e01804ab0abdc7a4b423b4
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-16 23:00:12 -0400

    Update app.js

--- aws/aws-blogs-api/aws-blogs-api/app.js
+++ aws/aws-blogs-api/aws-blogs-api/app.js
@@ -17,12 +17,8 @@ exports.lambdaHandler = async (event, context) => {
 
     try {
         const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                location: ret.data
-            })
-        }
+        response = ret.json()
+        
     } catch (err) {
         console.log(err);
         return err;
