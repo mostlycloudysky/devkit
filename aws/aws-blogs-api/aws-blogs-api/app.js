840335d8eaf58ad052be0145ed29c9a0e36ddde5
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-16 21:53:28 -0400

    fix aws blogs api and add pagination

--- aws/aws-blogs-api/aws-blogs-api/app.js
+++ aws/aws-blogs-api/aws-blogs-api/app.js
@@ -6,14 +6,20 @@ let response;
  */
 
 exports.lambdaHandler = async (event, context) => {
-    const url = "https://aws.amazon.com/api/dirs/items/search?item.directoryId=blog-posts&sort_by=item.additionalFields.createdDate&sort_order=desc&size=10&item.locale=en_US"
+
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+    }
+
+    const page = event.queryStringParameters.page || 1
+
+    const url = `https://aws.amazon.com/api/dirs/items/search?item.directoryId=blog-posts&sort_by=item.additionalFields.createdDate&sort_order=desc&size=10&item.locale=en_US&page=${page}`
 
     try {
         const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'AWS Blogs API Data',
                 location: ret.data
             })
         }
