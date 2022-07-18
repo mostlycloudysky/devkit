34f1be67ebf0d9d5fb9d20d7e5cc99525c31bd78
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-17 20:54:30 -0400

    Update app.js

--- aws/aws-blogs-api/aws-blogs-api/app.js
+++ aws/aws-blogs-api/aws-blogs-api/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                location: ret.data
+                data: ret.data
             })
         }
     } catch (err) {
