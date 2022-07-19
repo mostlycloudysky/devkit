6a53c4607e0693dc4def910fb63e269bc99916f6
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-19 16:07:00 -0400

    Update Aws.jsx

--- components/alltabs/Aws.jsx
+++ components/alltabs/Aws.jsx
@@ -18,7 +18,15 @@ function Aws() {
   }, []);
 
   if (isLoading) {
-    return <p>Loading....</p>;
+    return (
+      <>
+        <div className='flex items-center justify-center space-x-2 animate-bounce mt-5'>
+          <div className='w-8 h-8 bg-blue-400 rounded-full'></div>
+          <div className='w-8 h-8 bg-green-400 rounded-full'></div>
+          <div className='w-8 h-8 bg-black rounded-full'></div>
+        </div>
+      </>
+    );
   }
   if (!awsBlogs) {
     return <p>No List to show</p>;
