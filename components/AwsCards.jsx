3646459b54f2f5fec3346dc467a4fdd8266d58b6
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-19 15:20:43 -0400

    update card component

--- components/AwsCards.jsx
+++ components/AwsCards.jsx
@@ -25,85 +25,48 @@ function AwsCards({ blogs }) {
                     <div className='min-w-0 flex-1'>
                       <p className='text-sm font-medium text-gray-900'>
                         {blog.authors.map((author) => (
-                          <a
-                            key={blog.title}
-                            href='#'
-                            className='hover:underline'
-                          >
-                            Dries Vincent
+                          <a key={blog.title} href='#' className='mr-1'>
+                            {author}
                           </a>
                         ))}
 
-                        <time dateTime='2020-12-09T11:43:00'>, 07/18/2022</time>
+                        <time dateTime='2020-12-09'>
+                          , {new Date(blog.date_created).toLocaleDateString()}
+                        </time>
                       </p>
                     </div>
                   </div>
                 </div>
                 <div className='mt-2 text-sm text-gray-700 space-y-4'>
-                  <p>
-                    Jurassic Park was an incredible idea and a magnificent feat
-                    of engineering, but poor protocols and a disregard for human
-                    safety killed what could have otherwise been one of the best
-                    businesses of our generation.
-                  </p>
-                  <p>
+                  <p>{blog.post_excerpt}</p>
+                  {/* <p>
                     Ultimately, I think that if you wanted to run the park
                     successfully and keep visitors safe, the most important
                     thing to prioritize would be&hellip;
-                  </p>
+                  </p> */}
                 </div>
                 <div className='mt-6 flex justify-between space-x-8'>
                   <div className='flex space-x-6'>
                     <div>
                       <ul role='list' className='mt-2 leading-8'>
-                        <li className='inline'>
-                          <a
-                            href='#'
-                            className='relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5'
-                          >
-                            <div className='absolute flex-shrink-0 flex items-center justify-center'>
-                              <span
-                                className='h-1.5 w-1.5 rounded-full bg-rose-500'
-                                aria-hidden='true'
-                              />
-                            </div>
-                            <div className='ml-3.5 text-sm font-medium text-gray-900'>
-                              Bug
-                            </div>
-                          </a>{' '}
-                        </li>
-                        <li className='inline'>
-                          <a
-                            href='#'
-                            className='relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5'
-                          >
-                            <div className='absolute flex-shrink-0 flex items-center justify-center'>
-                              <span
-                                className='h-1.5 w-1.5 rounded-full bg-rose-500'
-                                aria-hidden='true'
-                              />
-                            </div>
-                            <div className='ml-3.5 text-sm font-medium text-gray-900'>
-                              Bug
-                            </div>
-                          </a>{' '}
-                        </li>
-                        <li className='inline'>
-                          <a
-                            href='#'
-                            className='relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5'
-                          >
-                            <div className='absolute flex-shrink-0 flex items-center justify-center'>
-                              <span
-                                className='h-1.5 w-1.5 rounded-full bg-indigo-500'
-                                aria-hidden='true'
-                              />
-                            </div>
-                            <div className='ml-3.5 text-sm font-medium text-gray-900'>
-                              Accessibility
-                            </div>
-                          </a>{' '}
-                        </li>
+                        {blog.categories.map((category) => (
+                          <li className='inline' key={blog.title}>
+                            <a
+                              href='#'
+                              className='relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5'
+                            >
+                              <div className='absolute flex-shrink-0 flex items-center justify-center'>
+                                <span
+                                  className='h-1.5 w-1.5 rounded-full bg-blue-500'
+                                  aria-hidden='true'
+                                />
+                              </div>
+                              <div className='ml-3.5 text-sm font-medium text-gray-900'>
+                                {category}
+                              </div>
+                            </a>{' '}
+                          </li>
+                        ))}
                       </ul>
                     </div>
                   </div>
