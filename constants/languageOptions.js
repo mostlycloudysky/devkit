ccf18521ac16da2a448fec11d1d9097b334b8e4b
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-18 22:28:22 -0400

    update card component for aws blogs

--- constants/languageOptions.js
+++ constants/languageOptions.js
@@ -1,20 +1,26 @@
 export const languageOptions = [
-    {
-      id: 63,
-      name: "JavaScript (Node.js 12.14.0)",
-      label: "JavaScript (Node.js 12.14.0)",
-      value: "javascript",
-    },
-    {
-      id: 45,
-      name: "Assembly (NASM 2.14.02)",
-      label: "Assembly (NASM 2.14.02)",
-      value: "assembly",
-    },      
-    {
-      id: 84,
-      name: "Visual Basic.Net (vbnc 0.0.0.5943)",
-      label: "Visual Basic.Net (vbnc 0.0.0.5943)",
-      value: "vbnet",
-    },
-  ];
+  {
+    id: 63,
+    name: 'JavaScript (Node.js 12.14.0)',
+    label: 'JavaScript (Node.js 12.14.0)',
+    value: 'javascript',
+  },
+  {
+    id: 45,
+    name: 'Assembly (NASM 2.14.02)',
+    label: 'Assembly (NASM 2.14.02)',
+    value: 'assembly',
+  },
+  {
+    id: 84,
+    name: 'Visual Basic.Net (vbnc 0.0.0.5943)',
+    label: 'Visual Basic.Net (vbnc 0.0.0.5943)',
+    value: 'vbnet',
+  },
+  {
+    id: 71,
+    name: 'Python (3.8.1)',
+    label: 'Python (3.8.1)',
+    value: 'python',
+  },
+];
