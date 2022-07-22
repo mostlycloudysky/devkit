9b180cd7290d1769dc44270433f4a7730a5d414f
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-22 11:48:10 -0400

    Update Aws.jsx

--- components/alltabs/Aws.jsx
+++ components/alltabs/Aws.jsx
@@ -4,18 +4,34 @@ import AwsCards from '../AwsCards';
 function Aws() {
   const [awsBlogs, setAwsBlogs] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   fetch(
-  //     'https://djawhtqabb.execute-api.us-east-1.amazonaws.com/Prod/awsblogs?page=1'
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setAwsBlogs(data); // Set the toDo variable
-  //       setIsLoading(false);
-  //     });
-  // }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://djawhtqabb.execute-api.us-east-1.amazonaws.com/Prod/awsblogs?page=${currentPage}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setAwsBlogs(data); // Set the toDo variable
+        setIsLoading(false);
+      });
+  }, [currentPage]);
+
+  // https://upmostly.com/tutorials/react-infinite-scroll
+  function handleScroll() {
+    var isAtBottom =
+      document.documentElement.scrollHeight -
+        document.documentElement.scrollTop <=
+      document.documentElement.clientHeight;
+
+    if (isAtBottom) {
+      // Load next posts
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
 
   if (isLoading) {
     return (
